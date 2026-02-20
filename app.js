f5efb640d2ec80c1442032a1adb6d8f2dc3a8691
Tommieboy99 <tomkoek99@hotmail.com>
2026-02-20 13:59:10 +0100

    added express body parsing middleware

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set("view engine", "ejs");
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+app.use(express.json());
+app.use(express.urlencoded( { extended: true} ));
 
 app.use("/", indexRouter);
 
