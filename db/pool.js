0d5a206d526c8221b592b451a3c9587caa2ff493
Tommieboy99 <tomkoek99@hotmail.com>
2026-02-24 16:10:02 +0100

    added connectionstring that checks if its railway connection if not falls back to local dev connection

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
 
+const connectionString = process.env.DATABASE_URL || process.env.DEV_DATABASE_URL;
+
 module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString,
 });
